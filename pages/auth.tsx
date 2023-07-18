d94c12a4e54c512846fa38b0051d4c8502fa04e5
Andrea Carvajal Osorio <andreacc9199@gmail.com>
2023-07-18 16:15:12 -0400

    github login works

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,7 +2,6 @@ import { useCallback, useState } from "react";
 import axios from 'axios';
 import Input from "../Components/input";
 import { signIn } from 'next-auth/react';
-import Router from 'next/router';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
@@ -22,10 +21,8 @@ const Auth = () => {
             await signIn('credentials', {
                 email, 
                 password,
-                redirect: false,
-                callbackUrl: '/',
+                callbackUrl: '/profiles',
             });
-            Router.push('/')
         } catch (error) {
             console.log(error);
         }
@@ -83,11 +80,11 @@ const Auth = () => {
                             {variant === "login" ? "Sign In" : "Sign Up"}
                         </button>
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-                            <div onClick={() => signIn('google', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                            <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                                 <FcGoogle size={30}/>
                             </div>
                             <div
-                                onClick={() => signIn('github', { callbackUrl: '/' })}
+                                onClick={() => signIn('github', { callbackUrl: '/profiles' })}
                                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition" >
                                 <FaGithub size={30}/>
                             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
+import { NextPageContext } from "next"
+import { getSession, signOut } from "next-auth/react"
+import useCurrentUser from "../hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return{
+    props: {}
+  }
+}
 
 const Home = () => {
+
+  const { data: user } = useCurrentUser()
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-green-500">Netflix clone</h1>
+      <h1 className="text-4xl font-bold text-green-500">Netflix clone</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
     </>
   )
 }
